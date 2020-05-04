8c39a2aaad18b157ea31b38f44a59d7e981024c3
Patrick Horne <35867466+Paddylabs@users.noreply.github.com>
2020-05-04 18:23:32 +0100

    COmmit

--- O365GroupEmailAddressReport.ps1
+++ O365GroupEmailAddressReport.ps1
@@ -44,7 +44,7 @@ Function Show-Menu {
 
 }
 
-#$UserCredential = Get-Credential
+#$UserCredential = Get-Credentials
 #$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 #Import-PSSession $Session -DisableNameChecking
 
